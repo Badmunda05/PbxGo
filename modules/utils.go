3be742c2b8dbffb166285cfd41e619d1dec98134
🕊️ ʙᴀᴅ ᴍᴜɴᴅᴀ ❤️ <sukhwinderwarval98@gmail.com>
2026-05-09 23:31:39 +0530

    Update utils.go

--- modules/utils.go
+++ modules/utils.go
@@ -2,10 +2,29 @@ package modules
 
 import "github.com/amarnathcjd/gogram/telegram"
 
-// Reply sends a message; if the sender is the bot itself it edits, otherwise replies.
-func Reply(m *telegram.NewMessage, text string) (*telegram.NewMessage, error) {
+// Reply sends a message;
+// if the sender is the bot itself it edits,
+// otherwise replies.
+
+func Reply(
+	m *telegram.NewMessage,
+	text string,
+) (*telegram.NewMessage, error) {
+
 	if m.SenderID() == m.Client.Me().ID {
-		return m.Edit(text, telegram.SendOptions{ParseMode: telegram.HTML})
+
+		return m.Edit(
+			text,
+			&telegram.SendOptions{
+				ParseMode: telegram.HTML,
+			},
+		)
 	}
-	return m.Reply(text, &telegram.SendOptions{ParseMode: telegram.HTML})
+
+	return m.Reply(
+		text,
+		&telegram.SendOptions{
+			ParseMode: telegram.HTML,
+		},
+	)
 }
