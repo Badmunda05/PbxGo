b756e3c1e43fa9f6d0f1deef04a222d4a10300a6
🕊️ ʙᴀᴅ ᴍᴜɴᴅᴀ ❤️ <sukhwinderwarval98@gmail.com>
2026-05-11 19:46:44 +0530

    Create test.go

---
+++ modules/bot/test.go
@@ -0,0 +1,29 @@
+package bot
+
+import (
+	"github.com/amarnathcjd/gogram/telegram"
+)
+
+// Simple test command
+func testHandler(m *telegram.NewMessage) error {
+
+	_, _ = m.Reply(
+		"✅ Bot Working Successfully!",
+		nil,
+	)
+
+	return nil
+}
+
+func init() {
+
+	modules.RegisterBot(modules.Module{
+		Name: "Test",
+		Commands: []modules.CommandDef{
+			{
+				Pattern: "test",
+				Handler: testHandler,
+			},
+		},
+	})
+}
